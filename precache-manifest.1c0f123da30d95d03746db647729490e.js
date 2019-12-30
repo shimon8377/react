9cadf87a4eac17bf253a296650f151c4480ea403
shimon <sc831977@gmail.com>
2019-12-30 21:07:10 +0200

    Updates

---
+++ precache-manifest.1c0f123da30d95d03746db647729490e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78c8fbff31f85e1aa975223867161314",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "0d22182627f2f45630df",
+    "url": "/react/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "375873901cf131bafab0",
+    "url": "/react/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0d22182627f2f45630df",
+    "url": "/react/static/js/2.96e50134.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/react/static/js/2.96e50134.chunk.js.LICENSE"
+  },
+  {
+    "revision": "375873901cf131bafab0",
+    "url": "/react/static/js/main.7407e299.chunk.js"
+  },
+  {
+    "revision": "5153198398d7602cedc3",
+    "url": "/react/static/js/runtime-main.021a889c.js"
+  }
+]);
